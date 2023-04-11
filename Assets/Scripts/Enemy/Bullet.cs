90b34779d03eb47a092bada74631b937906cb223
alperentnc <alprn_08_@hotmail.com>
2023-04-11 13:40:59 +0300

    add some

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -7,23 +7,32 @@ public class Bullet : MonoBehaviour
     public GameObject player;
     private Rigidbody2D rb;
     public float force;
-    private float timer;
+    private float timer,animTimer;
+    public Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
 
         Vector3 direction = player.transform.position - transform.position;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0,rot);
 
     }
 
     void Update()
     {
         timer += Time.deltaTime;
+        animTimer += Time.deltaTime;
         if (timer > 5)
         {
             Destroy(gameObject);
+        }
+        if (animTimer > .2f)
+        {
+            animator.SetBool("bulletStop", true);
         }
     }
 
