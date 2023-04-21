0b0f5c1afcb550481a301aecee201aec531a4f4b
ozcndogann <ozcndogann02@gmail.com>
2023-04-22 00:31:26 +0300

    PUSH

--- Assets/Scripts/Game/Door.cs
+++ Assets/Scripts/Game/Door.cs
@@ -7,13 +7,7 @@ public class Door : MonoBehaviour
     Adds adds;
     void Start()
     {
-        // Load the level that was last completed, or start at level 1 if none have been completed yet
-<<<<<<< Updated upstream
         adds = gameObject.GetComponent<Adds>();
-=======
-        adds = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Adds>();
-        
->>>>>>> Stashed changes
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -24,7 +18,6 @@ public class Door : MonoBehaviour
             adds.LoadFullSize();
             LevelsCoin.LevelCompleted = true;
             PlayerPrefs.SetInt("levelCompleted", SceneManager.GetActiveScene().buildIndex+1);
-            adds.LoadFullSize();
             // Load the next level
         }
     }
